937ba935669f7c739e2e4337e740d7b7355262b8
Valmyr Tavares <valmyr.lima@millennium.com.br>
2019-12-06 17:49:35 -0300

    405 Aqui foi colocado os filters para acrescentar os simbolos de dinheiro dentro do script

--- app.js
+++ app.js
@@ -3,19 +3,27 @@ const vm = new Vue({
   data: {
     pais: "Brazil",
     produtos: [],
+    produto: false,
+  },
+  filters: {
+    numeroPreco(valor) {
+      // return `R$ ${valor},00`
+      // return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }); em REAL 
+      return valor.toLocaleString("pt-BR", { style: "currency", currency: "USD" });
+    },
+    maiuscula(valor) {
+      return valor.toUpperCase();
+    }
   },
   methods: {
     fetchProdutos() {
       fetch("./api/produtos.json").then(r => r.json()).then(r => {
-
         this.produtos = r;
       })
     }
   },
   created() {
-
     this.fetchProdutos();
-
   }
 })
 
