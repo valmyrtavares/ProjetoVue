9ba3245808507ff184b0929a31d55058de378886
Valmyr Tavares <valmyr.lima@millennium.com.br>
2019-12-09 17:38:41 -0300

    408 Vai fechar o modal fazer uma função para qe feche e criar uma transição com o @keyframe para fazer a transição. Eu nunca vi nada assim

--- app.js
+++ app.js
@@ -25,6 +25,19 @@ const vm = new Vue({
       fetch(`./api/produtos/${id}/dados.json`).then(r => r.json()).then(r => {
         this.produto = r;
       })
+    },
+
+    abrirModal(id) {
+      this.fetchProduto(id);
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      })
+    },
+
+    fecharModal({ target, currentTarget }) {
+      if (target === currentTarget)
+        this.produto = false;
     }
   },
 
